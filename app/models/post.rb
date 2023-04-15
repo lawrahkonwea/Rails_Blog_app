f28be058cd101c56797f40a36edc1e14de313406
Amaka Konwea <lawrahcollections@gmail.com>
2023-04-15 13:09:19 +0100

    Create a method that updates post conter for users and five most recent comments

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  def update_posts_count
+    author.increment!(:posts_counter)
+  end
+
+  def recent_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+end
