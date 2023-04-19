4af493165dac32354ddae27f78c0a794aacbc062
lawrahkonwea <lawrahcollections@gmail.com>
2023-04-19 16:34:36 +0100

    Implement method for updating post counts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,14 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'user'
   has_many :likes
   has_many :comments
+
+def update_posts_count
+    author.increment!(:posts_counter)
+end    
+
+def recent_five_comments
+    comments.order(created_at: :desc).limit(5)
+end
 
 end
