bd30bb5a169187b1732797aa8e78da939ab57f09
lawrahkonwea <lawrahcollections@gmail.com>
2023-04-19 16:31:21 +0100

    Create method to retrieve recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
 
-  
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(3)
+end
 end
