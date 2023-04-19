82fe0a6d0b2e7aba6a63f4c1f418baaa5b2ade22
lawrahkonwea <lawrahcollections@gmail.com>
2023-04-19 17:01:30 +0100

    Add a like counter and fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :user
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
   def update_comment_count
     post.increment!(:comments_counter)
-end
-
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
 
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
-end
+  end
 end
