b582d3fbf0814111decf8de898425ada18056799
lawrahkonwea <lawrahcollections@gmail.com>
2023-04-20 18:49:52 +0100

    Create validation for post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,9 @@ class Post < ApplicationRecord
   def recent_five_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  validates :title, presense :true, length :{maximum: 250}
+  validates :comments_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0, message: 'must be an integer greater than or equal to zero.'}
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'must be an integer greater than or equal to zero.' }
+                          
 end
