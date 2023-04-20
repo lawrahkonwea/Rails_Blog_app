dc49fdcf24c5f3755ad8027d3c8494fbed9172a8
lawrahkonwea <lawrahcollections@gmail.com>
2023-04-20 18:36:10 +0100

    Create validation for user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
+
+  validates :name, presence :true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: 'must be an integer greater than or equal to zero'}
 end
