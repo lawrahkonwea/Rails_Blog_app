8279d705aa4ad3dbc432a29baceaa8935dc2d664
lawrahkonwea <lawrahcollections@gmail.com>
2023-04-29 18:09:24 +0100

    Create method to like posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,10 @@
 class LikesController < ApplicationController
-  def index; end
+  def create
+    @post = Post.find(params[:post_id])
+    @like = Like.new(post_id: @post.id, author_id: current_user.id)
+
+    return unless @like.save
+
+    redirect_to user_posts_path(@like.author_id, @like.post_id)
+  end
 end
