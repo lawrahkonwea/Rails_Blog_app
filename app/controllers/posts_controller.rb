c0d8980b2c212f135c801cfb97f8bf6ca6ce25bd
lawrahkonwea <lawrahcollections@gmail.com>
2023-05-01 23:04:15 +0100

    Fix n+1 query loading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(author_id: params[:user_id])
+    @posts = Post.includes(:author).where(author_id: params[:user_id])
     @user = User.includes(:posts).find(params[:user_id])
   end
 
