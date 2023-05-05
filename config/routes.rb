b1552b09f45908e8a71a041563d87d4c0c79e130
lawrahkonwea <lawrahcollections@gmail.com>
2023-05-05 05:27:22 +0100

    Migrate users and create login and signup inputs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get 'comments/index'
-  # get 'likes/index'
- 
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show, :new, :create] do
